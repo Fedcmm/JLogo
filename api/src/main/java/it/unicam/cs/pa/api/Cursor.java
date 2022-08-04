50950f35bfc7e38d3dc0fb348756181f79b34217
Fedcmm <40853168+Fedcmm@users.noreply.github.com>
2022-08-04 15:45:03 +0200

    Add basic Point class Add basic interface for cursors

---
+++ api/src/main/java/it/unicam/cs/pa/api/Cursor.java
@@ -0,0 +1,19 @@
+package it.unicam.cs.pa.api;
+
+import java.awt.Color;
+
+/**
+ * Represents a cursor in a Logo program
+ */
+public interface Cursor {
+
+    void move(Point point);
+
+    void setDirection(int direction);
+
+    void setPlotting(boolean plotting);
+
+    void setLineColor(Color color);
+
+    void setAreaColor(Color color);
+}
---
+++ api/src/main/java/it/unicam/cs/pa/api/Point.java
@@ -0,0 +1,10 @@
+package it.unicam.cs.pa.api;
+
+/**
+ * Holds coordinates for a point in a canvas
+ *
+ * @param x the x coordinate
+ * @param y the y coordinate
+ */
+public record Point(int x, int y) {
+}
